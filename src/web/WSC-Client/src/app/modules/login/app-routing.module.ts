f7c487e0d4ac3ed655a79328a1d2da4b95f8a0e2
Lucas Santos <lucas.sports@hotmail.com>
2021-11-28 16:11:14 -0300

    estrutura projeto WSC-Client

--- src/web/WSC-Client/src/app/modules/login/app-routing.module.ts
+++ src/web/WSC-Client/src/app/modules/login/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginAppComponent } from './components/login/login-app/login-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginAppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
