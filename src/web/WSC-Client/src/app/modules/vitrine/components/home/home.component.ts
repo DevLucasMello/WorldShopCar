218bfb72ea4f933cd3dc169c4284a4902d48b322
Lucas de Mello Santos <lucas.sports@hotmail.com>
2021-11-29 17:57:44 -0300

    vitrine inicio - Client

--- src/web/WSC-Client/src/app/modules/vitrine/components/home/home.component.ts
+++ src/web/WSC-Client/src/app/modules/vitrine/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Vitrine } from '../../models/vitrine';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  vitrine: Vitrine[] = [
+    {
+      id: "1",
+      caminho: "../../../../../assets/images/produtos/camiseta1.png",
+      nome: "Camiseta",
+      descricao: "Descrição do produto",
+      estoque: 3,
+      valor: 48
+    },
+    {
+      id: "2",
+      caminho: "../../../../../assets/images/produtos/camiseta2.png",
+      nome: "Camiseta",
+      descricao: "Descrição do produto",
+      estoque: 2,
+      valor: 59
+    },
+    {
+      id: "3",
+      caminho: "../../../../../assets/images/produtos/camiseta3.png",
+      nome: "Camiseta",
+      descricao: "Descrição do produto",
+      estoque: 8,
+      valor: 17
+    },
+    {
+      id: "4",
+      caminho: "../../../../../assets/images/produtos/camiseta4.png",
+      nome: "Camiseta",
+      descricao: "Descrição do produto",
+      estoque: 10,
+      valor: 142
+    },
+    {
+      id: "5",
+      caminho: "../../../../../assets/images/produtos/caneca1.png",
+      nome: "Camiseta",
+      descricao: "Descrição do produto",
+      estoque: 15,
+      valor: 39
+    },
+    {
+      id: "6",
+      caminho: "../../../../../assets/images/produtos/caneca2.png",
+      nome: "Camiseta",
+      descricao: "Descrição do produto",
+      estoque: 4,
+      valor: 107
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
---
+++ src/web/WSC-Client/src/app/modules/vitrine/models/vitrine.ts
@@ -0,0 +1,8 @@
+export interface Vitrine{
+    id: string;
+    caminho: string;
+    nome: string;
+    descricao: string;
+    estoque: number;
+    valor: number;
+  }
